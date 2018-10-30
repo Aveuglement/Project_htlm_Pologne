8f3835a22d853a2413809b1675451e4d18bbcc58
Intenable <Clara.lepesqueur@efrei.net>
2018-10-30 02:23:24 +0100

    Homework

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function greet() {
-    window.alert("Vous n'écoutez pas ce qu'on vous dit ma parole !");
+    window.alert("You're not listenning what you're told, my word!");
 }
 
 function init() {
@@ -14,7 +14,32 @@ function init() {
     images[3].src ="flower2.png";
 
     //      Task 3
+    document.getElementById("task3").innerHTML = "You are great!"; 
 
     //      Task 4
     let image = document.createElement("img");
+    image.src = "flower2.png";
+    task4.appendChild(image);
+
+    //      Task 5
+    let spans = document.getElementById("rainbow").getElementsByTagName("span"); 
+    let color = ["red", "orange","yellow","green","blue","purple","pink"];
+    for (var i = spans.length - 1; i >= 0; i--) 
+    {
+	    spans[i].style.color = color[i];
+    }
+
+    //      Task 6
+    var changeSrc = function(event) {
+        if (event.target.src) {
+            let filename = event.target.src.replace(/^.*[\\\/]/, '');
+            if (filename === "flower1.png") {
+                event.target.src = "flower2.png";
+            }
+            else {
+                event.target.src = "flower1.png";
+            }
+        }
+    }
+    document.getElementById("event").addEventListener("mouseover", changeSrc);
 }
